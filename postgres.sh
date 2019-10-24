363d80536be5ef67bedef67394689c7f66da65f5
akrherz <akrherz@iastate.edu>
2019-10-23 20:11:19 -0500

    initial PostgreSQL implementation

---
+++ postgres.sh
@@ -0,0 +1,26 @@
+###
+# Setup PostgreSQL 11 or 12 on Travis-CI(ubuntu xenial, bionic)
+
+# https://travis-ci.community/t/install-postgresql-11/3894/5
+
+# Setup ubuntugis repo
+#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+#echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" | sudo tee -a /etc/apt/sources.list.d/postgresql.list
+
+# Ensure repo info is current
+sudo apt-get -qq update
+
+sudo systemctl stop postgresql@10-main.service
+sudo apt-get -qq install -y --no-install-suggests --no-install-recommends postgresql-11-postgis-2.5-scripts postgresql-11 postgresql-client-11 postgresql-11-postgis-2.5
+sudo systemctl status postgresql@11-main.service
+sudo systemctl stop postgresql@11-main.service
+sudo sed -i 's/^port.*/port = 5432/' /etc/postgresql/11/main/postgresql.conf
+sudo grep port /etc/postgresql/11/main/postgresql.conf
+sudo cp /etc/postgresql/10/main/pg_hba.conf /etc/postgresql/11/main/pg_hba.conf
+sudo systemctl reload postgresql@11-main.service
+sudo systemctl start postgresql@11-main.service
+sudo systemctl status postgresql@11-main.service -l
+sudo cat /var/log/postgresql/postgresql-11-main.log
+
+export PATH="/usr/lib/postgresql/11/bin:$PATH"
+psql -h 127.0.0.1 -c 'CREATE ROLE travis SUPERUSER LOGIN CREATEDB;' -U postgres || true
