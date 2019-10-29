b788591321b533d8cf5ec0da59305f6e0004fa13
akrherz <akrherz@iastate.edu>
2019-10-29 14:04:55 -0500

    support mod-wsgi setup

---
+++ mod_wsgi.sh
@@ -0,0 +1,10 @@
+
+sudo apt-get install apache2 apache2-dev
+sudo a2enmod headers rewrite proxy cgi expires authz_groupfile
+
+MOD_WSGI_SO=$(find $HOME/miniconda/envs/prod -type f -name 'mod_wsgi*.so')
+echo $MOD_WSGI_SO
+echo "LoadModule wsgi_module $MOD_WSGI_SO" | sudo tee -a /etc/apache2/mods-enabled/wsgi.load > /dev/null
+echo "WSGIApplicationGroup %{GLOBAL}" | sudo tee -a /etc/apache2/mods-enabled/wsgi.load > /dev/null
+sudo service apache2 restart
+sudo systemctl status apache2.service -l
