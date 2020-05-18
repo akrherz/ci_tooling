f5670a86beeca423b992d5a1522460178f26c273
akrherz <akrherz@iastate.edu>
2020-05-18 07:33:41 -0500

    stop postgresql 9.3 service, just in case

--- postgres.sh
+++ postgres.sh
@@ -10,6 +10,8 @@
 # Ensure repo info is current
 sudo apt-get -qq update
 
+# https://travis-ci.community/t/bionic-postgresql-10-no-longer-starts-up/6002/13
+sudo systemctl stop postgresql@9.3-main.service
 sudo systemctl stop postgresql@10-main.service
 sudo systemctl status postgresql@10-main.service
 sudo apt-get -qq install -y --no-install-suggests --no-install-recommends postgresql-11-postgis-2.5-scripts postgresql-11 postgresql-client-11 postgresql-11-postgis-2.5
