96fce3e7c9d8ed245da23ea28101db0e4ea47011
daryl herzmann <akrherz@iastate.edu>
2020-11-27 07:14:15 -0600

    Github Actions (#7)
    * swing at Github Actions

--- postgres.sh
+++ postgres.sh
@@ -12,17 +12,17 @@ sudo apt-get -qq update
 
 # https://travis-ci.community/t/bionic-postgresql-10-no-longer-starts-up/6002/13
 sudo apt-get purge postgresql-9.3
-sudo systemctl stop postgresql@10-main.service
-sudo systemctl status postgresql@10-main.service
+sudo systemctl stop postgresql@10-main.service || true
+sudo systemctl status postgresql@10-main.service || true
 sudo apt-get -qq install -y --no-install-suggests --no-install-recommends postgresql-11-postgis-2.5-scripts postgresql-11 postgresql-client-11 postgresql-11-postgis-2.5
-sudo systemctl status postgresql@11-main.service
-sudo systemctl stop postgresql@11-main.service
+sudo systemctl status postgresql@11-main.service || true
+sudo systemctl stop postgresql@11-main.service || true
 # https://travis-ci.community/t/postgres-default-port-changed-from-5432-to-5433/7347/10
 sudo sed -i "s/^data_directory.*/data_directory = \'\/var\/lib\/postgresql\/11\/main\/\'/" /etc/postgresql/11/main/postgresql.conf
 sudo sed -i 's/^port.*/port = 5432/' /etc/postgresql/11/main/postgresql.conf
 sudo grep port /etc/postgresql/11/main/postgresql.conf
-sudo cp /etc/postgresql/10/main/pg_hba.conf /etc/postgresql/11/main/pg_hba.conf
-sudo systemctl reload postgresql@11-main.service
+sudo cp postgres_files/pg_hba.conf /etc/postgresql/11/main/pg_hba.conf
+sudo systemctl reload postgresql@11-main.service || true
 sudo systemctl start postgresql@11-main.service
 sudo systemctl status postgresql@11-main.service -l
 sudo cat /var/log/postgresql/postgresql-11-main.log
