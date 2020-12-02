35852f76d11bd02f7ac815871eacec6105b57ba1
daryl herzmann <akrherz@iastate.edu>
2020-12-01 21:32:25 -0600

    migrate more fully to Github Actions (#9)
    * migrate more fully to Github Actions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ci_tooling
+ci_tooling
+==========
+
 Shared Continuous Integration Tooling
+
+Limited integration testing is done on Github Actions: [![Build Status](https://github.com/akrherz/ci_tooling/workflows/Install%20and%20Test/badge.svg)](https://github.com/akrherz/ci_tooling)
--- miniconda.sh
+++ miniconda.sh
@@ -2,13 +2,14 @@
 export PATH="$HOME/miniconda/bin:$PATH"
 
 if [ ! -f $HOME/miniconda/envs/prod/bin/python ]; then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
     bash miniconda.sh -f -b -p $HOME/miniconda
     . $HOME/miniconda/etc/profile.d/conda.sh
     conda config --set quiet True --set always_yes yes --set changeps1 no
     conda config --prepend channels conda-forge
     conda config --set channel_priority strict
-    conda create -n prod python=$PYTHON_VERSION --file conda_requirements.txt
+    echo Installing Python Version ${PYTHON_VERSION}
+    conda create -n prod python=${PYTHON_VERSION} --file conda_requirements.txt
     conda activate prod
     conda clean -y --all -q
     python -m pip install --upgrade -r pip_requirements.txt
