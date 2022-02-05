a01f84a0923fce697da6bba010e34e94635cb087
akrherz <akrherz@iastate.edu>
2022-02-05 07:24:44 -0600

    fix path

--- iemwebfarm.sh
+++ iemwebfarm.sh
@@ -3,4 +3,4 @@
 sudo mkdir /opt/iemwebfarm
 sudo git clone https://github.com/akrherz/iemwebfarm.git /opt/iemwebfarm
 
-sudo cp /opt/iemwebfarm/config/iemwebfarm.conf /etc/apache2/sites-enabled/
+sudo cp /opt/iemwebfarm/apache_conf.d/iemwebfarm.conf /etc/apache2/sites-enabled/
