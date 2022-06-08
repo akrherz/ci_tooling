ef6c865a9c724713c80b88beeb2c5f817e03869f
akrherz <akrherz@iastate.edu>
2022-06-08 12:56:09 -0500

    use mamba

--- miniconda.sh
+++ miniconda.sh
@@ -1,6 +1,6 @@
 # Get conda in our $PATH
 export PATH="$HOME/miniconda/bin:$PATH"
-export MF="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh"
+export MF="https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh"
 
 if [ ! -f $HOME/miniconda/envs/prod/bin/python ]; then
     wget -q $MF -O miniconda.sh
@@ -8,9 +8,9 @@ if [ ! -f $HOME/miniconda/envs/prod/bin/python ]; then
     . $HOME/miniconda/etc/profile.d/conda.sh
     conda config --set quiet True --set always_yes yes --set changeps1 no
     echo Installing Python Version ${PYTHON_VERSION}
-    conda create -n prod python=${PYTHON_VERSION} --file conda_requirements.txt
+    mamba create -n prod python=${PYTHON_VERSION} --file conda_requirements.txt
     conda activate prod
-    conda clean -y --all -q
+    mamba clean -y --all -q
     python -m pip install --upgrade -r pip_requirements.txt
     rm -rf $HOME/miniconda/pkgs/cache/*
 fi
