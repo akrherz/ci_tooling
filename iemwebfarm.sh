8c07425e65235e25a4d6a32f404e283c4a207bb9
akrherz <akrherz@iastate.edu>
2024-01-17 21:45:32 -0600

    hack at LD_PRELOAD for apache2, sigh

--- iemwebfarm.sh
+++ iemwebfarm.sh
@@ -4,3 +4,7 @@ sudo mkdir /opt/iemwebfarm
 sudo git clone https://github.com/akrherz/iemwebfarm.git /opt/iemwebfarm
 
 sudo cp /opt/iemwebfarm/apache_conf.d/iemwebfarm.conf /etc/apache2/sites-enabled/
+
+sudo mkdir /etc/systemd/system/apache2.service.d
+sudo cp systemd/apache2_override.conf /etc/systemd/system/apache2.service.d/override.conf
+sudo systemctl daemon-reload
