0c461a256cd890bd5e4e6257f99e5cf48d281075
akrherz <akrherz@iastate.edu>
2024-02-07 13:48:32 -0600

    fix path

--- mod_wsgi.sh
+++ mod_wsgi.sh
@@ -3,7 +3,7 @@ sudo apt-get install apache2 apache2-dev
 sudo a2enmod headers rewrite proxy cgi expires authz_groupfile
 python -m pip install --upgrade mod-wsgi
 
-MOD_WSGI_SO=$(find $HOME/miniconda/envs/prod -type f -name 'mod_wsgi*.so')
+MOD_WSGI_SO=$(find $HOME/micromamba/envs/prod -type f -name 'mod_wsgi*.so')
 echo $MOD_WSGI_SO
 echo "LoadModule wsgi_module $MOD_WSGI_SO" | sudo tee -a /etc/apache2/mods-enabled/wsgi.load > /dev/null
 echo "WSGIApplicationGroup %{GLOBAL}" | sudo tee -a /etc/apache2/mods-enabled/wsgi.load > /dev/null
